a2cbe73db8b7e1b86d7f212d01c51144ac8ab3f4
Karol Rosiński <markar.toster@gmail.com>
2018-11-05 18:09:29 +0100

    add header and db api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Game from './Game';
 
 class App extends Component {
+
+  state = {
+    movies: []
+  }
+
+  async componentDidMount() {
+    try {
+      const res = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=a9a044fab959ff29628041fff2fcac7b&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1');
+      const movies = await res.json();
+      this.setState({
+        movies: movies.results,
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <header className="App-header">Wisielec</header>
+        <Game></Game>
       </div>
     );
   }
