6e3940bf25fca13f4d9b73341fc9be0ac2c1de9d
Karol Rosiński <markar.toster@gmail.com>
2018-11-05 18:48:11 +0100

    add single random word display

--- src/Game.js
+++ src/Game.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
 
 class Game extends Component {
+
+  state = {
+    movie: "",
+    movies: []
+  }
+
+  async componentDidMount() {
+    try {
+      const res = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=a9a044fab959ff29628041fff2fcac7b&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1');
+      const movies = await res.json();
+      this.setState({
+        movie: movies.results[Math.round(Math.random() * (19 - 0) + 0)].title,
+        movies: movies.results 
+      });
+      console.log(movies.results);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
   render() {
+
+    const { movie } = this.state;
+
     return (
       <div>
-        
+        <h1>{movie}</h1>
       </div>
     );
   }
