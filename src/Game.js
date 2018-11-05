3def3f2e86d9cc2fa718881c956765ce17fcc440
Karol Rosiński <markar.toster@gmail.com>
2018-11-05 19:17:50 +0100

    add style to word display

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import './App.css';
 
 class Game extends Component {
 
   state = {
-    movie: "",
+    movie: [],
     movies: []
   }
 
@@ -12,7 +13,7 @@ class Game extends Component {
       const res = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=a9a044fab959ff29628041fff2fcac7b&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1');
       const movies = await res.json();
       this.setState({
-        movie: movies.results[Math.round(Math.random() * (19 - 0) + 0)].title,
+        movie: movies.results[Math.round(Math.random() * (19 - 0) + 0)].title.toUpperCase().split(''),
         movies: movies.results 
       });
       console.log(movies.results);
@@ -27,7 +28,7 @@ class Game extends Component {
 
     return (
       <div>
-        <h1>{movie}</h1>
+        {movie.map(letter => <div className="cell">{letter}</div>)}
       </div>
     );
   }
