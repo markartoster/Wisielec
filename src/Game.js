27df0b02ca6efd263b4dcc0326ebe844ebb2f59f
Karol Rosiński <markar.toster@gmail.com>
2018-11-06 17:34:58 +0100

    add new ui elements and fix display bugs

--- src/Game.js
+++ src/Game.js
@@ -4,6 +4,7 @@ import './App.css';
 class Game extends Component {
 
   state = {
+    lifes: 10,
     movie: [],
     movies: []
   }
@@ -13,7 +14,8 @@ class Game extends Component {
       const res = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=a9a044fab959ff29628041fff2fcac7b&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1');
       const movies = await res.json();
       this.setState({
-        movie: movies.results[Math.round(Math.random() * (19 - 0) + 0)].title.toUpperCase().split(''),
+        //movie: movies.results[Math.round(Math.random() * (19 - 0) + 0)].title.toUpperCase().split(''),
+        movie: movies.results[7].title.toUpperCase().split(''),
         movies: movies.results 
       });
       console.log(movies.results);
@@ -24,20 +26,33 @@ class Game extends Component {
 
   render() {
 
-    const { movie } = this.state;
+    const { movie, lifes } = this.state;
     let letterCell = []; 
 
     movie.map(letter => {
-      if(letter !== ' ')
+      if(letter !== ' ' && letter !== ':' && letter !== '-')
         letterCell.push((<div className="cell">{letter}</div>))
+      else if (letter === ':' || letter === '-')
+        letterCell.push((<div className="empty-cell">{letter}</div>))
       else
         letterCell.push((<div className="empty-cell"></div>))
     })
 
 
     return (
-      <div>
-        {letterCell.map(letter => letter)}
+      <div className="game">
+        <div className="lifes">Lifes:</div>
+        <div className="life-number">
+          {lifes}
+        </div>
+        <div>
+          {letterCell.map(letter => letter)}
+        </div>
+        <div className="input-container">
+          <input className="input" type="text"></input>
+        </div>
+        <button>Click</button>
+        <div className="picked-letters"></div>
       </div>
     );
   }
