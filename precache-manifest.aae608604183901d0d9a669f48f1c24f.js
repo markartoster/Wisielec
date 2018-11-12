8f06b61cc5ede7ad48c0784ea8501841fa7de5cd
Karol Rosiński <markar.toster@gmail.com>
2018-11-12 20:51:38 +0100

    Updates

---
+++ precache-manifest.aae608604183901d0d9a669f48f1c24f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba37dd50e00f841b2c18",
+    "url": "/Wisielec/static/js/runtime~main.ba37dd50.js"
+  },
+  {
+    "revision": "6fe498724446fd5825a8",
+    "url": "/Wisielec/static/js/main.6fe49872.chunk.js"
+  },
+  {
+    "revision": "3c84e96e6bf2dd28fd07",
+    "url": "/Wisielec/static/js/1.3c84e96e.chunk.js"
+  },
+  {
+    "revision": "6fe498724446fd5825a8",
+    "url": "/Wisielec/static/css/main.0b69c628.chunk.css"
+  },
+  {
+    "revision": "3c84e96e6bf2dd28fd07",
+    "url": "/Wisielec/static/css/1.a89637ac.chunk.css"
+  },
+  {
+    "revision": "909eb979219d216f46f7eb2b50240e1f",
+    "url": "/Wisielec/index.html"
+  }
+];
